5e9aeb7a9876efefed06b3102d0a9afd92a03fd2
Arild Birkeland <arildbirkeland@Arilds-MacBook-Pro.local>
2017-04-27 08:41:55 +0200

    basic web project added

--- toysproject/src/main/java/com/arildbir/PageController.java
+++ toysproject/src/main/java/com/arildbir/PageController.java
@@ -2,6 +2,7 @@ package com.arildbir;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +37,8 @@ public class PageController {
    //     this.notificationService = notificationService;
     //}
 
-    @RequestMapping
+    //@RequestMapping
+    //@Controller
     public String home () {
         //return notificationService.toString();
         //return pageControllerMsg;
---
+++ toysproject/src/main/resources/static/js/app.js
@@ -0,0 +1,1 @@
+console.log ("app.js logger til consollen....");
